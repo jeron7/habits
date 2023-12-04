d48416a2c8b8cc50bc69d077bec7e60e7057d959
jeron7 <jeronimojairoo@gmail.com>
2023-12-04 20:11:18 -0300

    Create unit tests for LocalDateUtils

--- backend/src/main/java/habits/backend/utils/LocalDateUtils.java
+++ backend/src/main/java/habits/backend/utils/LocalDateUtils.java
@@ -6,6 +6,8 @@ import java.time.temporal.WeekFields;
 public class LocalDateUtils {
 
     public static LocalDate getDateOfDaysAgo(int days) {
+        assert days > 0  : "days must be greater than zero";
+
         LocalDate today = LocalDate.now();
 
         return today.minusDays(days);
